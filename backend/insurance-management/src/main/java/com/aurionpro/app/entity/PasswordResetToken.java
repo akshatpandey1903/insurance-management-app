181462f1608cc7932435abc98c77b9a8773a4e44
poonawala2303 <mustafa.poonawala82@gmail.com>
2025-04-03 13:58:16 +0530

    Add Forgot Password and Reset Functionality

--- backend/insurance-management/src/main/java/com/aurionpro/app/entity/PasswordResetToken.java
+++ backend/insurance-management/src/main/java/com/aurionpro/app/entity/PasswordResetToken.java
@@ -1,5 +1,39 @@
 package com.aurionpro.app.entity;
 
-public class PasswordResetToken {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name= "password_reset_token")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class PasswordResetToken 
+{
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long pass_id;
+	
+	@Column
+	private String token;
+	
+	@OneToOne
+	@JoinColumn(name="id",nullable=false)
+	private User user;
+	
+	@Column
+	private LocalDateTime expiryDate;
+	
 }
