fd10b726f4956985683be658c1636189ac11de1c
akshatpandey1903 <akshatp1903@gmail.com>
2025-04-04 12:21:40 +0530

    update

--- backend/insurance-management/src/main/java/com/aurionpro/app/service/PasswordResetTokenServiceImpl.java
+++ backend/insurance-management/src/main/java/com/aurionpro/app/service/PasswordResetTokenServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
